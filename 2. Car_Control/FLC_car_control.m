c9a71a81c5bd97b5f7459ba09ee8137ee2ffa0c9
Iasonas Pavlidis <iasonaspav@gmail.com>
2021-10-14 03:08:27 +0300

    Add FLC car control assets dir

--- 2. Car_Control/FLC_car_control.m
+++ 2. Car_Control/FLC_car_control.m
@@ -27,6 +27,9 @@ plotmf(flc,'input',ind_theta)
 figure;
 plotmf(flc,'output',ind_dTheta)
 
+figure;
+plotmf(flc2,'output',ind_dTheta)
+
 %% Simulate body motion
 close all;
 
@@ -40,35 +43,41 @@ obs_x = [5 5 6 6 7 7 10];
 obs_y = [0 1 1 2 2 3 3];
 
 thetas = [0,-45,-90];
-for i = 1:length(thetas)
-    xi = x0;
-    yi = y0;
-    route = [xi yi];
-    theta = thetas(i);
-    d = [];
-    while (xi <= x_des)
-        [dH,dV] = get_dist(xi,yi,obs_x,obs_y);
-        d = [d; [xi yi dH dV]];
-        dTheta = evalfis([dV dH theta],flc2);
-        theta = theta + dTheta;
-        if (theta > 180); theta = theta - 360; end
-        if (theta < -180); theta = theta + 360; end
-        xi = xi + v*cosd(theta);
-        yi = yi + v*sind(theta);
-        route = [route ; [xi yi]];
-    end
+simulate_car(x0,y0,x_des,y_des,v,obs_x,obs_y,thetas,flc);
+fprintf("\n\n");
+simulate_car(x0,y0,x_des,y_des,v,obs_x,obs_y,thetas,flc2);
 
-    fprintf("Final position: (%f,%f)\n",xi,yi);
-    figure;
-    plot(obs_x,obs_y);
-    xlim([0 10]);
-    ylim([0 4]);
-    hold on;
-    plot(route(:,1),route(:,2));
-    grid;
-    plot(x0,y0,'*');
-    plot(x_des,y_des,'*');
-    title("Initial theta = " + thetas(i) + " degrees");
+function simulate_car(x0,y0,x_des,y_des,v,obs_x,obs_y,thetas,fuzzy_controller)
+    for i = 1:length(thetas)
+        xi = x0;
+        yi = y0;
+        route = [xi yi];
+        theta = thetas(i);
+        d = [];
+        while (xi <= x_des)
+            [dH,dV] = get_dist(xi,yi,obs_x,obs_y);
+            d = [d; [xi yi dH dV]];
+            dTheta = evalfis([dV dH theta],fuzzy_controller);
+            theta = theta + dTheta;
+            if (theta > 180); theta = theta - 360; end
+            if (theta < -180); theta = theta + 360; end
+            xi = xi + v*cosd(theta);
+            yi = yi + v*sind(theta);
+            route = [route ; [xi yi]];
+        end
+
+        fprintf("Final position: (%f,%f)\n",xi,yi);
+        figure;
+        plot(obs_x,obs_y);
+        xlim([0 10]);
+        ylim([0 4]);
+        hold on;
+        plot(route(:,1),route(:,2));
+        grid;
+        plot(x0,y0,'*');
+        plot(x_des,y_des,'*');
+        title("Initial theta = " + thetas(i) + " degrees");
+    end
 end
 
 
